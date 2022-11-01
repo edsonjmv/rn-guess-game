c2d9dd848a4706c66f89cc93dea39be771ca0827
MACHE022 <edson.machado.-nd@disney.com>
2022-11-01 16:22:50 +0100

    Adding game control buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 
 function generateRandomBetween(min, max, exclude) {
@@ -13,9 +14,40 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 function GameScreen({ userNumber }) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong', [
+        { text: 'Sorry!', style: 'cancel' }
+      ]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  }
 
   return (
     <View style={styles.screen}>
@@ -23,6 +55,14 @@ function GameScreen({ userNumber }) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
+        <View style={styles.buttons}>
+          <PrimaryButton onPress={() => nextGuessHandler('lower')}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler('greater')}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View></View>
     </View>
@@ -33,7 +73,8 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24
-  }
+  },
+  buttons: {}
 });
 
 export default GameScreen;
